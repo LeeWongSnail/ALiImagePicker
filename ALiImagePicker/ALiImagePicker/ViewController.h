b9c3327aef4f80e4eb01879596d70ddad3fb081d
LeeWongSnail <@wangli_0632@163.com>
2016-10-14 14:51:15 +0800

    init

---
+++ ALiImagePicker/ALiImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALiImagePicker
+//
+//  Created by LeeWong on 2016/10/14.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ALiImagePicker/ALiImagePickerTests/ALiImagePickerTests.m
@@ -0,0 +1,39 @@
+//
+//  ALiImagePickerTests.m
+//  ALiImagePickerTests
+//
+//  Created by LeeWong on 2016/10/14.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ALiImagePickerTests : XCTestCase
+
+@end
+
+@implementation ALiImagePickerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
